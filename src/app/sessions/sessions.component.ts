20e6175f55003302d1c048860f0daf1622141cf6
Alfred-Onuada <aonuada5@gmail.com>
2024-05-24 02:13:26 +0100

    loading done on sessions

--- src/app/sessions/sessions.component.ts
+++ src/app/sessions/sessions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
 import { ISession } from '../interfaces/session';
@@ -16,9 +16,13 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
     styleUrls: ['./sessions.component.css'],
     imports: [CommonModule, HeaderComponent, RouterLink, MatDialogModule]
 })
-export class SessionsComponent implements OnInit {
+export class SessionsComponent {
     sessions: ISession[] = []
     sessionsSub$!: Subscription;
+
+    loading = true;
+    showError = false;
+    errorMessage = "";
 
     constructor(
         private titleService: Title,
@@ -26,23 +30,33 @@ export class SessionsComponent implements OnInit {
         public dialog: MatDialog
     ) {
         this.titleService.setTitle("Sessions - Babcock University School of Law and Security Studies");
-    }
 
-    openSessionModal() {
-        this.dialog.open(AddSessionModalComponent);
-    }
-
-    ngOnInit(): void {
         this.sessionsSub$ = this.sessionService.getSessions().subscribe({
             next: data => {
                 this.sessions = data;
+
+                this.loading = false;
             },
             error: (error) => {
                 console.error(error);
+
+                this.showError = true;
+                this.errorMessage = "An error occurred while fetching sessions. Please try again later.";
+
+                setTimeout(() => {
+                    this.showError = false;
+                    this.errorMessage = "";
+
+                    this.loading = false;
+                }, 3000);
             },
             complete: () => {
                 this.sessionsSub$.unsubscribe();
             }
         })
+    }
+
+    openSessionModal() {
+        this.dialog.open(AddSessionModalComponent);
     }
 }
