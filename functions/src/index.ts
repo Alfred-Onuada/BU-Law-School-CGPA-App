367037b52e448f22c21636a393e86d5c3a1a7929
Alfred-Onuada <aonuada5@gmail.com>
2024-06-11 17:19:11 +0100

    updated build script to prep for prod deployment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import apiRoutes from './routes/api.routes';
 import cors from 'cors';
+import path from 'node:path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(
   cors({
@@ -12,7 +14,11 @@ app.use(
 );
 
 async function main() {
-  app.use('', apiRoutes);
+  app.get('', (_, res) => {
+    res.sendFile('public/index.html', { root: __dirname });
+  });
+  
+  app.use('api', apiRoutes);
 
   app.all('*', (_, res) => {
     res.status(404).json({ message: 'There is nothing here. 404 Not Found' });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: "https://law-school-api.onrender.com"
+  apiUrl: "https://law-school-api.onrender.com/api"
 };
