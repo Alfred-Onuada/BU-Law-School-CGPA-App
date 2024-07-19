02fd811420433f3ec61e3213ba37b7ea6e3d8db7
Alfred-Onuada <aonuada5@gmail.com>
2024-07-19 16:24:07 +0100

    update to show grades for summer

--- src/app/interfaces/all-students-list.ts
+++ src/app/interfaces/all-students-list.ts
@@ -8,14 +8,20 @@ export interface IAllStudentsList {
   currentCGPA: number;
   first100: number;
   second100: number;
+  summer100: number;
   first200: number;
   second200: number;
+  summer200: number;
   first300: number;
   second300: number;
+  summer300: number;
   first400: number;
   second400: number;
+  summer400: number;
   first500: number;
   second500: number;
+  summer500: number;
   first600: number;
   second600: number;
+  summer600: number;
 }
